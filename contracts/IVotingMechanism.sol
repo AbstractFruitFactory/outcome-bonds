40b393e8720cbcb33d1af0246db00c368728d166
alexmedkex <alexmedkex@gmail.com>
2018-02-26 20:07:22 +0100

    Changed some voting logic and added concrete voting class

--- contracts/IVotingMechanism.sol
+++ contracts/IVotingMechanism.sol
@@ -5,9 +5,9 @@ import '../node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 contract IVotingMechanism {
     enum Vote { UNKNOWN, MET, NOT_MET }
     
-    function checkVote(address _subject) constant returns (Vote result) { return Vote.UNKNOWN; }
-    function checkCap(ERC20 _payoutToken, address _subject, address _topic, uint _value) constant returns (bool allowed);
-    function vote(address _subject, address _topic, Vote _vote);
+    function checkVote(address _topic) constant returns (Vote result) { return Vote.UNKNOWN; }
+    function checkCap(ERC20 _payoutToken, address _topic, uint _value) constant returns (bool allowed);
+    function vote(address _topic, Vote _vote);
 
-    event Voted(address indexed _subject, address indexed _topic, Vote _vote);
+    event Voted(address indexed _topic, Vote _vote);
 }
